e54d6b8c4770dc56c5cee3e068056465fec4598c
michaelfame <michaelfame@gmail.com>
2023-10-08 01:32:53 +0100

    addedd the function for the license badge in the generateMarkdown.js

--- index.js
+++ index.js
@@ -5,8 +5,60 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? 📧'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Where What is your Github User id?'
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: "What is your Project's Name?"
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is your Project About..describe briefly?'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license is needed for the project?',
+        choices:["LGPLv3", "MIT","GPLv3","APACHE Ver 2.0"] 
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What prequiste commands needs to be run for dependencies to be installed',
+        default: `npm i`
+      },
+     
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user needs to know about using the readme generator Repo?'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What commands needs to be used to run test',
+        default: `npm test`
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user needs to know in order to contribute to this REPO? '
+      }
+    ];
+  
+  const generateHTML = (answers) =>
 
-];
+;
 
 // function to write README file
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // function to generate markdown for README
+// funcition to generate the license to be used in the readme.md file
+function renderLicenseBadge(license) {
+  if (license !== 'None'){
+  return `![${license}](https://img.shields.io/badge/${license}-blueviolet)`;  }
+  return ``
+}
+// function that returns a license link from user input and generates the correct badge based on their selection
+
+
+
+
+
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
 
 `;
 }
