e8eea2b42826af75370c19d5815781fb99b58e09
michaelfame <michaelfame@gmail.com>
2023-10-10 14:29:09 +0100

    worked on the generateMarkdown.js file to show the arrnagement for the readme and its functions

--- README.md
+++ README.md
@@ -1,4 +1,59 @@
-# Readme-Generator
+# readme-generator
   ![MIT](https://img.shields.io/badge/MIT-blueviolet)
+
+  ### Description
+  using nodejs to create a readme file through the CLI
+
+  ### Table Of Content
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  
+* [License](#license)
+
+
+  * [Contributing](#contributing)
+  * [Test](#tests)
+  * [Questions](#questions)
+  * 
+  ## Installation
+  
+  To install necessary npm dependencies, run this command:
+
+  !*!*!*
+  npm i  
+  !*!*!*
+
+  ## Usage
+  node version higher than 16, all npm dependencies must be installed
+
+  ## License
+  
+  This project is licensed under the MIT license.
+
+  ## Contributing
+
+  pull request
+
+  ## Tests
+  
+  Run the follwoing commands to run tests:
+  
+  !*!*!*
+  npm test  
+  !*!*!*
+
+  ### Questions
+  If you have any questions about the repo or want to open a issue, please contact me 
+  at my GitHub account at https://github.com/undefined. 
+  
+  You can also email me directly at michaelfame@gmail.com.
+
+
+  
+  
+
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,21 @@
 // function to generate markdown for README
 // funcition to generate the license to be used in the readme.md file
-function renderLicenseBadge(license) {
+const renderLicenseBadge = (license) =>  {
   if (license !== 'None'){
   return `![${license}](https://img.shields.io/badge/${license}-blueviolet)`;  }
   return ``
 }
 // function that returns a license link from user input and generates the correct badge based on their selection
+
+const renderLinkToLicense = (license) => {
+  if (license !=="None"){
+    return `\n* [License](#license)\n`;
+    }
+  return "";
+  };
+
+
+
 
 
 
@@ -16,6 +26,59 @@ function renderLicenseBadge(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
+
+  ### Description
+  ${data.description}
+
+  ### Table Of Content
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  ${renderLinkToLicense(data.license)}
+
+  * [Contributing](#contributing)
+  * [Test](#tests)
+  * [Questions](#questions)
+  * 
+  ## Installation
+  
+  To install necessary npm dependencies, run this command:
+
+  !*!*!*
+  ${data.installation}  
+  !*!*!*
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+  
+  Run the follwoing commands to run tests:
+  
+  !*!*!*
+  ${data.test}  
+  !*!*!*
+
+  ### Questions
+  If you have any questions about the repo or want to open a issue, please contact me 
+  at my GitHub account at https://github.com/${data.github}. 
+  
+  You can also email me directly at ${data.email}.
+
+
+  
+  
+
 
 
 `;
